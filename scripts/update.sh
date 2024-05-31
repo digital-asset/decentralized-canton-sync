2d4bd67ccf135137f2accd007ddf885704d6f1b6
Itai Segall <itai.segall@digitalasset.com>
2024-05-31 09:14:25 -0400

    copy daml code also to splice (#2)

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,14 +2,26 @@
 
 set -eou pipefail
 
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <CN_REPO_ROOT> <SPLICE_REPO_ROOT>"
+    exit 1
+fi
+
 CN_REPO_ROOT=$1
+SPLICE_REPO_ROOT=$2
+
+function copy_daml {
+    target="$2"
+    for pkg in $pkgs; do
+        src="$1/$pkg"
+        rsync -av --delete "$src"  --exclude '.daml' --include '*/' --include 'daml/Splice/*' --include '*.yaml' --include '*.md' --exclude '*' "$target"
+        find "$target" -empty -type d -delete
+    done
+}
 
 pkgs=$(ls -I "dars*" -I "splitwell*" $CN_REPO_ROOT/daml)
 
-for pkg in $pkgs; do
-    src="$CN_REPO_ROOT/daml/$pkg"
-    rsync -av --delete "$src"  --exclude '.daml' --include '*/' --include 'daml/Splice/*' --include '*.yaml' --include '*.md' --exclude '*' daml/
-done
+copy_daml $CN_REPO_ROOT/daml daml
 
 COPYRIGHT_ESCAPED='-- Copyright (c) 2024 Digital Asset (Switzerland) GmbH and\/or its affiliates\. All rights reserved\.\
 -- SPDX-License-Identifier: Apache-2\.0\
@@ -17,3 +29,5 @@ COPYRIGHT_ESCAPED='-- Copyright (c) 2024 Digital Asset (Switzerland) GmbH and\/o
 '
 
 find daml -type f -name \*.daml -exec bash -c 'grep -q -e "-- Copyright" "$0" || sed -i "1s/^/$1/" $0' '{}' "$COPYRIGHT_ESCAPED" \;
+
+copy_daml daml $SPLICE_REPO_ROOT/daml
