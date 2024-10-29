b9083a2c5c1a8a6dcf9deaa0fd667b6776f13ea3
Stephen Compall <stephen.compall@daml.com>
2024-10-29 18:19:34 +0000

    add issue references [ci]

--- scripts/check-repo-names.sh
+++ scripts/check-repo-names.sh
@@ -26,21 +26,21 @@ function check_patterns_locally() {
     'clean-cn|cn-clean' # TODO (#15706) rename to clean-splice/splice-clean
     'cn-component' # TODO (#15707) rename label in charts to splice-component
     '^[^:]+(grafana-dashboards|WalletMetricsTest).*cn[._]|MetricName."cn"' # TODO (#15708) replace metrics prefix cn with splice
-    'grafana-dashboards.*("(title|description)": "(CN)|by CN Apps)' # TODO () grafana descriptions
-    'grafana-dashboards.*("cn"|"cn,daml")' # TODO () grafana cn query identifiers
-    'grafana-dashboards.*postgres_exporter\.json:.*"pge-cn-apps-pg-scan-sv-3"' # TODO () grafana pulumi pg reference
+    'grafana-dashboards.*("(title|description)": "CN|by CN Apps)' # TODO (#15737) grafana descriptions
+    'grafana-dashboards.*("cn"|"cn,daml")' # TODO (#15738) grafana cn query identifiers
+    'grafana-dashboards.*postgres_exporter\.json:.*"pge-cn-apps-pg-scan-sv-3"' # TODO (#15746) grafana pulumi pg reference
     'databaseName = "cn_apps"|"cn_apps_reonboard"' # TODO (#15709) rename database to splice_apps
     '(Chart-template\.yaml|helm/.*/NOTES.txt):.*Canton Network' # TODO (#15710) remove Canton Network from helm descriptions 
     '^[^:]+V001__create_schema\.sql:' # TODO (#15491) avoiding changing hashes
-    'AUTH0_CN_MANAGEMENT_API_CLIENT_(ID|SECRET)|"dev" => ."AUTH0_CN"' # TODO () auth0 env names
+    'AUTH0_CN_MANAGEMENT_API_CLIENT_(ID|SECRET)|"dev" => ."AUTH0_CN"' # TODO (#15747) auth0 env names
     'cluster/compose/validator/.*\.yaml' # TODO (#14303) old base-version
-    '/helm/.*(Values\.ingress\.cns|cns: |name: cns-|- "cns\.)' # TODO () cns in splice-cluster-ingress-runbook
-    'ans-web-ui\.yaml:.*\$cnsWebUiLabel' # TODO () $cnsWebUiLabel in splice-validator template
-    'ans-web-ui\.yaml:.*name: splice-app-cns-ui-auth' # TODO () new secret
-    'Headers.scala:.*"cn-svc-configs"' # TODO () references to missing project subdirectory
-    'package-lock\.json:.*("cns/frontend"|"name": "cns-frontend"|"cns/openapi-ts-client/dist"|"name": "cns-external-openapi")' # TODO () remove old versions from package-lock if possible
-    '(Validator|Sv)App\.scala:.*"canton network.*realm"' # TODO () remove 'canton network' from realm names
-    'istio-gateway/.*gateway\.yaml:.*credentialName: cn-' # TODO () TLS credential names in istio-gateway
+    '/helm/.*(Values\.ingress\.cns|cns: |name: cns-|- "cns\.)' # TODO (#15739) cns in splice-cluster-ingress-runbook
+    'ans-web-ui\.yaml:.*\$cnsWebUiLabel' # TODO (#15740) $cnsWebUiLabel in splice-validator template
+    'ans-web-ui\.yaml:.*name: splice-app-cns-ui-auth' # TODO (#15741) new secret
+    'Headers.scala:.*"cn-svc-configs"' # TODO (#15742) references to missing project subdirectory
+    'package-lock\.json:.*("cns/frontend"|"name": "cns-frontend"|"cns/openapi-ts-client/dist"|"name": "cns-external-openapi")' # TODO (#15743) remove old versions from package-lock if possible
+    '(Validator|Sv)App\.scala:.*"canton network.*realm"' # TODO (#15744) remove 'canton network' from realm names
+    'istio-gateway/.*gateway\.yaml:.*credentialName: cn-' # TODO (#15745) TLS credential names in istio-gateway
     'GcpConfig\.scala:' # cluster-specific
     '/da-cn-shared/cn-images|GOOGLE_CLOUD_PROJECT=da-cn-shared' # gcp
     '/cn-release-bundles' # docs route
