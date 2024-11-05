67a7aacac6c410a9bbd5c72c1b3b426a49d4b5f8
DA Automation <splice-maintainers@digitalasset.com>
2024-11-05 16:36:47 +0000

    Update Splice from CCI
    Signed-off-by: DA Automation <splice-maintainers@digitalasset.com>

--- scripts/check-repo-names.sh
+++ scripts/check-repo-names.sh
@@ -3,9 +3,18 @@
 # Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+# shellcheck disable=SC2016
+
 set -euo pipefail
 
+REPO_ROOT=$( git rev-parse --show-toplevel )
+copy_script="scripts/copy-to-splice.sh"
 rename_script="scripts/check-repo-names.sh"
+if [[ -f "$REPO_ROOT/$copy_script" ]]; then
+  in_copy_src=yes
+else
+  in_copy_src=no
+fi
 
 function check_patterns_locally() {
   # removing NEVERMATCHES alternative causes these to never match
@@ -23,22 +32,19 @@ function check_patterns_locally() {
   local exceptions=(
     '(\b|[`_])cn-docs'
     '@cn-load-tester\.com'
-    'clean-cn|cn-clean' # TODO (#15706) rename to clean-splice/splice-clean
     'cn-component' # TODO (#15707) rename label in charts to splice-component
     '^[^:]+(grafana-dashboards|WalletMetricsTest).*cn[._]|MetricName."cn"' # TODO (#15708) replace metrics prefix cn with splice
     'grafana-dashboards.*("(title|description)": "CN|by CN Apps)' # TODO (#15737) grafana descriptions
     'grafana-dashboards.*("cn"|"cn,daml")' # TODO (#15738) grafana cn query identifiers
     'grafana-dashboards.*postgres_exporter\.json:.*"pge-cn-apps-pg-scan-sv-3"' # TODO (#15746) grafana pulumi pg reference
     'databaseName = "cn_apps"|"cn_apps_reonboard"' # TODO (#15709) rename database to splice_apps
-    '(Chart-template\.yaml|helm/.*/NOTES.txt):.*Canton Network' # TODO (#15710) remove Canton Network from helm descriptions 
+    '(Chart-template\.yaml|helm/.*/NOTES.txt):.*Canton Network' # TODO (#15710) remove Canton Network from helm descriptions
     '^[^:]+V001__create_schema\.sql:' # TODO (#15491) avoiding changing hashes
     'AUTH0_CN_MANAGEMENT_API_CLIENT_(ID|SECRET)|"dev" => ."AUTH0_CN"' # TODO (#15747) auth0 env names
     'cluster/compose/validator/.*\.yaml' # TODO (#14303) old base-version
     '/helm/.*(Values\.ingress\.cns|cns: |name: cns-|- "cns\.)' # TODO (#15739) cns in splice-cluster-ingress-runbook
-    'ans-web-ui\.yaml:.*\$cnsWebUiLabel' # TODO (#15740) $cnsWebUiLabel in splice-validator template
     'ans-web-ui\.yaml:.*name: splice-app-cns-ui-auth' # TODO (#15741) new secret
     'Headers.scala:.*"cn-svc-configs"' # TODO (#15742) references to missing project subdirectory
-    'package-lock\.json:.*("cns/frontend"|"name": "cns-frontend"|"cns/openapi-ts-client/dist"|"name": "cns-external-openapi")' # TODO (#15743) remove old versions from package-lock if possible
     '(Validator|Sv)App\.scala:.*"canton network.*realm"' # TODO (#15744) remove 'canton network' from realm names
     'istio-gateway/.*gateway\.yaml:.*credentialName: cn-' # TODO (#15745) TLS credential names in istio-gateway
     'GcpConfig\.scala:' # cluster-specific
@@ -83,14 +89,28 @@ function check_patterns_locally() {
 }
 
 function setup_temp_splice() {
-  local src="$1" tempsplice="$(mktemp -d)"
+  local src="$1" tempsplice
+  tempsplice="$(mktemp -d)"
   cd "$src"
-  direnv exec . scripts/copy-to-splice.sh "$tempsplice"
+  local script_prefix
+  case $in_copy_src in
+    yes) script_prefix=;;
+    no) script_prefix='direnv exec .';;
+  esac
+  $script_prefix "$copy_script" "$tempsplice"
   cd "$tempsplice"
 }
 
 function check_patterns() {
-  while getopts 'hs:' arg; do
+  local optstring
+  case "$in_copy_src" in
+    yes)
+      optstring='h'
+      setup_temp_splice "$REPO_ROOT";;
+    no) optstring='hs:';;
+  esac
+
+  while getopts "$optstring" arg; do
     case "$arg" in
       h)
         echo '  Options: [-s SPLICE_REPO]
